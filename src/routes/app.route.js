b0e73fb36928185704f4d20f16c985e5186929d4
Nixomart <nixoma1@gmail.com>
2022-10-31 17:00:35 -0300

    edicion de tareas

--- src/routes/app.route.js
+++ src/routes/app.route.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import Task from "../models/Task";
 const router = Router();
 
+//muestra de tareas
 router.get("/", async (req, res) => {
   const tasks = await Task.find().lean();
   //tasksList sera el array de obj que se enviariá
   res.render("index", { tasksList: tasks });
 });
 
+//listar tareas
 router.post("/task/add", async (req, res) => {
   try {
     const task = Task(req.body);
@@ -19,11 +21,27 @@ router.post("/task/add", async (req, res) => {
     console.log(error);
   }
 });
-router.get("/about", (req, res) => {
-  res.render("about");
+
+//ruta para ir ala edicion de tarea
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error.message);
+  }
 });
-router.get("/editing", (req, res) => {
-  res.render("editing");
+
+//edicion de tareas
+router.post("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Task.findByIdAndUpdate(id, req.body);
+    res.redirect("/");
+    
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 export default router;
