3d7e5d682cbe7196ac3d3be3c4f76efd4a68d207
Nixomart <nixoma1@gmail.com>
2022-10-30 22:29:43 -0300

    agregando el modelo de tarea, Schema

---
+++ src/models/Task.js
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const TaskSchema = new Schema(
+  {
+    title: {
+      require: true,
+      type: String,
+      unique: true,
+      //trim es una funcion de javascrip para quitar los espacio sen blanco
+      trim: true,
+    },
+    description: {
+      require: true,
+      type: String,
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timeStamps: true, versionKey: false }
+);
+
+//mongoose, por defualt add __v propierty like version key, we dont use
+
+export default model("Task", TaskSchema);
